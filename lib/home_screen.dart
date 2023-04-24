9a1bf3ef333433b2a3b9907d66f9a9115d990c99
Piotr <Piofor.0106@gmail.com>
2023-04-24 19:42:39 +0200

    home&quiz screens added

---
+++ lib/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  void onPressed() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300, color: const Color.fromARGB(150, 255, 255, 255)),
+          const SizedBox(height: 48),
+          const Text(
+            'Show your knowledge!',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+          const SizedBox(height: 16),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text(
+              'Start Quiz!',
+              style: TextStyle(fontSize: 16),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
